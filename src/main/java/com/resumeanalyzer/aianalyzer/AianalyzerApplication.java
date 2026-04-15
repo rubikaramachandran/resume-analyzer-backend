d4497155df9030e95c564619d9187deaddf4a274
rubikaramachandran <146564732+rubikaramachandran@users.noreply.github.com>
2026-04-15 15:01:44 +0530

    Add files via upload

---
+++ src/main/java/com/resumeanalyzer/aianalyzer/AianalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.resumeanalyzer.aianalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AianalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AianalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumeanalyzer/aianalyzer/AianalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeanalyzer.aianalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AianalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
